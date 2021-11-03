102646d9b3ccbec64263099d95f12b82a3415130
Tima <rootspain@gmail.com>
2021-11-03 20:50:31 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Test ionic web-intent
+1) npm install
+2) 
+3) 
+4) click single button on single page "Start Intent"
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'message/:id',
-    loadChildren: () => import('./view-message/view-message.module').then( m => m.ViewMessagePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+  
 ];
 
 @NgModule({

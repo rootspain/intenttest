1ba77d268920291bef433fc2d7f7da66919415a0
Tima <rootspain@gmail.com>
2021-11-03 20:49:34 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IntentOptions, WebIntent } from '@ionic-native/web-intent/ngx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  private intentOptions:IntentOptions = {
+    action: this.webIntent.ACTION_VIEW,
+    url: 'tel:1234567890'
+  }
+
+
+
+  constructor(private webIntent: WebIntent) {}
+
+  public onStartIntentClick() {
+    console.log('clicked');
+    this.webIntent.sendBroadcast(this.intentOptions);
+  }
 }
